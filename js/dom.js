c1e37a7c8060ecacc5a8c53e0ceda690ad8d50d2
Stefan Caky <stefan.caky777@gmail.com>
2021-02-19 23:23:46 +0000

    addImgToCard
    Added more helper functions to dom.js to create inputs, form, img tags ...  Created addImgToCard(data) function.

--- js/dom.js
+++ js/dom.js
@@ -3,23 +3,48 @@ const dom = {
     searchContainer: document.querySelector('.search-container'),
     gallery: document.querySelector('#gallery'),
 
-    createElement(el, classname, idname, src) {
+    createElement(el, classname, text) {
         const element = document.createElement(el);
         element.className = classname;
-        if(idname) {
-            element.id = idname;
+        if(text) {
+            element.textContent = text;
         }
-        if(src) {
-            element.src = src;
-        }
+        
         return element;
     },
 
     getElement(el) {
         const element = document.querySelector(el);
         return element;
-    }
+    },
 
+    createForm(method) {
+        const element = document.createElement('form');
+        element.action = '#';
+        element.method = method;
+
+        return element;
+    },
+
+    createInput(type, id, classname, placeholder) {
+        const element = document.createElement('input');
+        element.type = type;
+        element.id = id;
+        element.className = classname;
+        if(placeholder) {
+            element.placeholder = placeholder;
+        }
+        
+        return element;
+    },
+
+    createImg(classname, src, alt){
+        const element = document.createElement('img');
+        element.className = classname;
+        element.src = `${src}`;
+        element.alt = alt;
+        return element;
+    }
 
 }
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,32 @@
 const randomUrl = 'https://randomuser.me/api/?results=12';
 
-// createElement(el, classname, idname, src)
+dom.searchContainer.appendChild(dom.createForm('GET'));
+
+dom.getElement('form').appendChild(dom.createInput('search', 'search-input', 'search-input', 'Search...'))
+
+dom.getElement('form').appendChild(dom.createInput('submit', 'search-submit', 'search-submit'))
+
+dom.getElement('#search-submit').value = 'Search';
+
+dom.gallery.after(dom.createElement('div', '"modal-container'));
+
+// dom.gallery.appendChild(dom.createElement('div', 'card'));
+
+// dom.getElement('.card').appendChild(dom.createElement('div', 'card-img-container'));
+
+
+
+
+
+// createElement(el, classname, text)
+// createInput(type, id, classname, obj, value)
+// createImg(classname, src, alt)
 
 // body: document.getElementsByTagName('body'),
 // searchContainer: document.querySelector('.search-container'),
 // gallery: document.querySelector('#gallery'),
+
+//dom.getElement('.card-img-container').appendChild(dom.createImg('card-img', data.results[2].picture.medium, 'gf' ))
 
 
 
@@ -14,4 +36,41 @@ function fetchData(url){
         .then(resp => resp.json())
 }
 
+
+fetchData(randomUrl)
+    .then(data => {
+        addImgToCard(data)
+    } )
+
+
+function addImgToCard(data) {
+    const cardImg = data.results.map(user => {
+
+
+        let image = (dom.createImg('card-img', user.picture.large, `${user.picture.large}` ))
+        let imgContainer = dom.createElement('div', 'card-img-container');
+        let card = dom.createElement('div', 'card');
+
+        imgContainer.appendChild(image);
+        card.appendChild(imgContainer);
+        dom.gallery.appendChild(card);
+
+
+        let cardInfoContainer = dom.createElement('div', 'card-info-container');
+        let firstLastNames = dom.createElement('h3', 'card-name', `${user.name.first} ${user.name.last}`);
+        let email = dom.createElement('p', 'card-text', `${user.email}`);
+        let city = dom.createElement('p', 'card-text', `${user.location.city}`);
+        cardInfoContainer.appendChild(firstLastNames);
+        cardInfoContainer.appendChild(email);
+        cardInfoContainer.appendChild(city);
+
+
+        imgContainer.after(cardInfoContainer);
+        
+    })
+}
+
+function modal(data) {
+    
+}
 
