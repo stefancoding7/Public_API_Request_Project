245bc7272e46c5e02640d4263ca0dae89f4ff50c
Stefan Caky <stefan.caky777@gmail.com>
2021-02-20 01:04:56 +0000

    modal function
    Added some helper function for formating. Modal function not 100 percent but is working

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ dom.getElement('form').appendChild(dom.createInput('submit', 'search-submit', 's
 
 dom.getElement('#search-submit').value = 'Search';
 
-dom.gallery.after(dom.createElement('div', '"modal-container'));
+
 
 // dom.gallery.appendChild(dom.createElement('div', 'card'));
 
@@ -31,9 +31,10 @@ dom.gallery.after(dom.createElement('div', '"modal-container'));
 
 
 
-function fetchData(url){
-   return fetch(url)
+async function fetchData(url){
+   return await fetch(url)
         .then(resp => resp.json())
+        .catch(error => console.log('Some problem. Cant resole the request', error))
 }
 
 
@@ -41,36 +42,104 @@ fetchData(randomUrl)
     .then(data => {
         addImgToCard(data)
     } )
+    
+    
 
 
 function addImgToCard(data) {
     const cardImg = data.results.map(user => {
 
 
-        let image = (dom.createImg('card-img', user.picture.large, `${user.picture.large}` ))
-        let imgContainer = dom.createElement('div', 'card-img-container');
-        let card = dom.createElement('div', 'card');
+        const image = (dom.createImg('card-img', user.picture.large, `${user.picture.large}` ))
+        const imgContainer = dom.createElement('div', 'card-img-container');
+        const card = dom.createElement('div', 'card');
 
         imgContainer.appendChild(image);
         card.appendChild(imgContainer);
         dom.gallery.appendChild(card);
 
 
-        let cardInfoContainer = dom.createElement('div', 'card-info-container');
-        let firstLastNames = dom.createElement('h3', 'card-name', `${user.name.first} ${user.name.last}`);
-        let email = dom.createElement('p', 'card-text', `${user.email}`);
-        let city = dom.createElement('p', 'card-text', `${user.location.city}`);
-        cardInfoContainer.appendChild(firstLastNames);
+        const cardInfoContainer = dom.createElement('div', 'card-info-container');
+        const name = dom.createElement('h3', 'card-name', `${user.name.first} ${user.name.last}`);
+        name.id = 'name';
+        const email = dom.createElement('p', 'card-text', `${user.email}`);
+        const city = dom.createElement('p', 'card-text', `${user.location.city}`);
+        cardInfoContainer.appendChild(name);
         cardInfoContainer.appendChild(email);
         cardInfoContainer.appendChild(city);
 
 
         imgContainer.after(cardInfoContainer);
         
+
+        card.addEventListener('click', (e) => {
+            
+            modal(user);
+            
+        })
     })
 }
 
-function modal(data) {
+
+
+function modal(user) {
     
+    dom.gallery.after(dom.createElement('div', 'modal-container'));
+    
+    dom.getElement('.modal-container').appendChild(dom.createElement('div', 'modal'))
+   
+    dom.getElement('.modal').appendChild(dom.createElement('button', 'modal-close-btn'));
+    let button = dom.getElement('.modal-close-btn');
+    button.appendChild(dom.createElement('strong', '', 'X'));
+    button.id = 'modal-close-btn';
+    button.addEventListener('click', () => {
+        removeModal()
+    });
+
+    button.after(dom.createElement('div', 'modal-info-container'));
+    const modalInfoContainer = dom.getElement('.modal-info-container');
+    const image = dom.createImg('modal-img', user.picture.large, `${user.picture.large}`)
+    const name = dom.createElement('h3', 'modal-name cap', `${user.name.first} ${user.name.last}`)
+    name.id = 'name';
+    const email = dom.createElement('p', 'modal-text', `${user.email}`);
+    const city = dom.createElement('p', 'card-text', `${user.location.city}`);
+    
+    const hr = document.createElement('hr');
+    
+    const phone = dom.createElement('p', 'modal-text', `${formatingPhone(user.phone)}`);
+    const location = dom.createElement('p', 'card-text', `${user.location.street.number} ${user.location.street.name} ${user.location.city}`);
+    const birthday = dom.createElement('p', 'card-text', `${formatingBirthday(user.dob.date)}`);
+    modalInfoContainer.appendChild(image);
+    modalInfoContainer.appendChild(name);
+    modalInfoContainer.appendChild(email);
+    modalInfoContainer.appendChild(city);
+    modalInfoContainer.appendChild(hr);
+    modalInfoContainer.appendChild(phone);
+    modalInfoContainer.appendChild(location);
+    modalInfoContainer.appendChild(birthday);
 }
 
+function removeModal() {
+    dom.getElement('.modal-container').remove();
+}
+
+function formatingPhone(number) {
+    
+    let phone = number.toString().replace(/[^\d]/g,'');
+    phone = phone.replace(/^([0-9]{3})([0-9]{3})([0-9]{1,})$/,'($1) $2-$3');
+    console.log(phone);
+    return phone
+}
+
+function formatingBirthday(string) {
+    let st;
+
+    if(string.length > 10) {
+        st = string.substring(0,10);
+    }
+    st = st.split("").reverse().join("")
+    st = st.replace(/[^\d]/g, '/');
+
+    
+    return st;
+}
